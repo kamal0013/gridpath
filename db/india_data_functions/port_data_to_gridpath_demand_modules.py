66a21bf90c46fdcac873b8f85bfe83e2368df827
ranjitster <ranjit.deshmukh@berkeley.edu>
2019-11-11 19:05:21 -0800

    functions for project and demand data porting to sql for India but I have stuck all these in the main port file.

--- db/india_data_functions/port_data_to_gridpath_demand_modules.py
+++ db/india_data_functions/port_data_to_gridpath_demand_modules.py
@@ -1,4 +1,5 @@
-
+#!/usr/bin/env python
+# Copyright 2017 Blue Marble Analytics LLC. All rights reserved.
 
 """
 Modules to import load data
@@ -6,6 +7,7 @@ Modules to import load data
 from collections import OrderedDict
 import pandas as pd
 from db.utilities import system_load
+
 
 def create_loads():
     """
@@ -24,7 +26,7 @@ def create_loads():
     load = load_all[(load_all.index.isin(year_month_days_selected.index))].reset_index()
 
     # Write to csv
-    #load.to_csv(inputFolder + 'demand_hourly_selected.csv')
+    # load.to_csv(inputFolder + 'demand_hourly_selected.csv')
 
     # Reset index for year month day
     year_month_days_selected = year_month_days_selected.reset_index()
@@ -36,10 +38,8 @@ def create_loads():
     load_annual = load_annual.drop(['month', 'day', 'hour'], axis=1).reset_index()
 
 
-
 def load_loads():
     """
-
     :return:
     """
 
@@ -48,14 +48,14 @@ def load_loads():
     for z in load.columns.to_list()[4:]:
         print(z)
         load_zone = load[z]
-        load_zone = pd.concat([load_zone, period_horizon_timepoints['tmp']], axis = 1)
+        load_zone = pd.concat([load_zone, period_horizon_timepoints['tmp']], axis=1)
         load_zone[['tmp']] = load_zone[['tmp']].astype(int)
         load_zone = load_zone.set_index(['tmp'])
         load_zone_dict = load_zone[z].to_dict()
         # Add stage id TODO: modify to be flexible in adding multiple stages
         load_zone_stage_dict = OrderedDict()
         load_zone_stage_dict[1] = load_zone_dict
-        #load_zone_stage_dict = {1: load_zone_dict}
+        # load_zone_stage_dict = {1: load_zone_dict}
         load_dict[z] = load_zone_stage_dict
 
     # Insert data
@@ -64,8 +64,9 @@ def load_loads():
         load_scenario_id=1,
         scenario_name='base_profile_2014_forecast',
         scenario_description='Base profile forecast using 2014 base load',
-        zone_stage_timepoint_static_loads = load_dict
+        zone_stage_timepoint_static_loads=load_dict
     )
+
 
 if __name__ == "__main__":
     pass
--- db/india_data_functions/port_data_to_gridpath_project_modules.py
+++ db/india_data_functions/port_data_to_gridpath_project_modules.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# Copyright 2017 Blue Marble Analytics LLC. All rights reserved.
 
 """
 Modules to import project data
